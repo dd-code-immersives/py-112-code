3c9f2a2adeb1714f606fa30253f046948c54b8c5
Dominic Doyle <andrewdoyle@Dominics-MacBook-Air.local>
2021-06-15 14:41:25 -0400

    added pyramid problem

---
+++ exercises/pyramid_problem.py
@@ -0,0 +1,19 @@
+"""
+write a program that prints the following
+```
+*
+* *
+* * *
+* * * *
+* * * * *
+* * * * * 
+* * * *
+* * *
+* *
+*
+``
+1) do it using a for loop 
+2) do it using a list comp. 
+part 2:
+replace the middle of any odd numbered row with a smilie emoji 😊 in the middle
+"""
