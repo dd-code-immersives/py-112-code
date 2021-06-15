2cfb28e85b9dd835575f5035e47cff8e02249a4d
Dominic Doyle <andrewdoyle@Dominics-MacBook-Air.local>
2021-06-15 10:13:39 -0400

    added exercises

---
+++ exercises/ascii_calculator.py
@@ -0,0 +1,7 @@
+"""
+ASCII (American Standard Code for Information Interchange), is a character encoding standard for electronic communication. ASCII codes represent text in computers, telecommunications equipment, and other devices.
+There is a function for finding the ascii value of a character here: https://docs.python.org/3/library/functions.html
+Write a function that asks the user for input, and then computes the sum of all the ascii values of the characters.
+"hello" -> 104 + 101 + 108 + 108 + 111 = 532
+Part 2: Given a list of words (from user input) determine which word has the highest ascii value.
+"""
---
+++ exercises/double_letter.py
@@ -0,0 +1,9 @@
+""" 
+Analyze a string to check if it contains two of the same letter in a row. For example, the string "hello" has l twice in a row, while the string "nono" does not have two identical letters in a row.
+Define a function named double_letters that takes a single parameter. The parameter is a string. Your function must return True if there are two identical letters in a row in the string, and False otherwise.
+Sample Inputs:
+hello -> True
+nono -> False
+sunday -> False
+racecar -> False
+"""
