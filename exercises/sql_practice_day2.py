7758650943246a48f28d663fb35f6da66bb42551
dd-code-immersives <domilamad89@gmail.com>
2021-06-19 14:11:44 -0400

    updated with part 2

--- exercises/sql_practice_day2.py
+++ exercises/sql_practice_day2.py
@@ -3,10 +3,10 @@ import csv, sqlite3
 
 from pprint import pprint
 
-def main_solution():
+def read_inventory_file(filename='../sql-data/inventory.csv'):
 
 
-	with open('../sql-data/inventory.csv','r') as fin:
+	with open(filename,'r') as fin:
 		dr = csv.DictReader(fin)
 		all_vals = []
 		for entry in dr:
@@ -14,9 +14,10 @@ def main_solution():
 			price_quantity = round(float(entry['price']) * int(entry['quantity']), 2)
 			csv_vals.append(price_quantity)
 			all_vals.append(tuple(csv_vals))
-		print(all_vals)
+	return all_vals
 		
 
+def run_execute_many(vals_to_insert):
 	#create database, add new table, drop table if already there.	
 	con = sqlite3.connect("hardware_store.db") # change to 'sqlite:///your_filename.db'
 	cur = con.cursor()
@@ -29,14 +30,30 @@ def main_solution():
 	   	price_extended REAL)
 	""")
 
-	cur.executemany("""INSERT INTO inventory VALUES (?,?,?,?,?)""", all_vals )
+	cur.executemany("""INSERT INTO inventory VALUES (?,?,?,?,?)""", vals_to_insert )
 	cur.execute("""SELECT * from inventory""")
 	data = cur.fetchall()
 	pprint(data) 
+	return data 
+
+def generate_sql_from_input(n=3):
+
+	#amount of entries to input
+	#split string by comma, cast to tuple
+	rows = []
+	while n > 0:
+		data = tuple(input("Enter Row Data: ").split(','))
+		rows.append(data)
+		n -= 1
+
+	return rows 
 
 
 if __name__ == '__main__':
-	main_solution()
+	# for second part 
+	# data = generate_sql_from_input()
+	data = read_inventory_file()
+	run_execute_many(data)
 
 
 
